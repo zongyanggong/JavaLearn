ae443297186bbbf3cbeb2e7b4b31fd003f51b3eb
zongyanggong <zongyanggong@gmail.com>
2022-12-07 11:14:20 -0500

    Revert "Second idea commit"
    This reverts commit 4c645b09

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Second");
-
-        System.out.println("Third");
     }
 }
