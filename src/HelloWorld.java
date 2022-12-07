26a058e3edfde119e01627d25e8dfa6a52c7dd8e
zongyanggong <zongyanggong@gmail.com>
2022-12-07 11:11:13 -0500

    Third idea commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Second");
+
+        System.out.println("Third");
     }
 }
