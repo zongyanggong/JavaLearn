4ed93c003f493a2e1d6a771b57ce5ecc04e60676
zongyanggong <zongyanggong@gmail.com>
2022-12-17 16:53:51 -0500

    test String

---
+++ wrapperclass/src/com/study/TestString.java
@@ -0,0 +1,11 @@
+package com.study;
+
+public class TestString {
+    public static void main(String[] args) {
+        String s1="hello";
+        System.out.println(s1);
+        s1 = "java";
+        System.out.println(s1);
+
+    }
+}
