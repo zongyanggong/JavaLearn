befc7793fe13be72a9fb13ef3de7c4c3a20c315b
zongyanggong <zongyanggong@gmail.com>
2023-01-03 14:55:48 -0500

    EvenAndPositive

--- PC1/src/EvenAndPositive.java
+++ PC1/src/EvenAndPositive.java
@@ -3,15 +3,19 @@
  * Student ID: 2295104
  */
 
+import java.util.Scanner;
+
 public class EvenAndPositive {
     public static void main(String[] args) {
-        int number = Integer.parseInt(args[0]);
+        System.out.print("> run EvenAndPositive ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
         boolean even = (number % 2 ==0);
         boolean positive = (number>0);
 
-        System.out.println(number + " is an even number: "+even);
-        System.out.println(number + " is a positive number: "+positive);
-        System.out.println(number + " is a positive even number: "+ (positive && even));
+        System.out.println(" " + number + " is an even number: "+even);
+        System.out.println(" " + number + " is a positive number: "+positive);
+        System.out.println(" " + number + " is a positive even number: "+ (positive && even));
 
     }
 }
